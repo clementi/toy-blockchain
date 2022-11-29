d3de6268b57739c41405dec97f43a04d8633c964
Jeffrey Pratt <clementi@users.noreply.github.com>
2022-11-29 00:00:11 -0700

    Use "chain" instead of "self"

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -32,43 +32,43 @@ func New() *blockchain {
 	}
 }
 
-func (self *blockchain) NewBlock(proof string) *block {
-	return self.NewBlockWithHash(proof, "")
+func (chain *blockchain) NewBlock(proof string) *block {
+	return chain.NewBlockWithHash(proof, "")
 }
 
-func (self *blockchain) NewBlockWithHash(proof string, previousHash string) *block {
+func (chain *blockchain) NewBlockWithHash(proof string, previousHash string) *block {
 	prevHash := previousHash
 
 	if len(prevHash) == 0 {
-		prevHash = Hash(self.LastBlock())
+		prevHash = Hash(chain.LastBlock())
 	}
 
 	block := block{
-		Index:        len(self.Chain) + 1,
+		Index:        len(chain.Chain) + 1,
 		Timestamp:    time.Now().UnixNano(),
-		Transactions: self.CurrentTransactions,
+		Transactions: chain.CurrentTransactions,
 		Proof:        proof,
 		PreviousHash: prevHash,
 	}
 
-	self.CurrentTransactions = make([]transaction, 0)
-	self.Chain = append(self.Chain, block)
+	chain.CurrentTransactions = make([]transaction, 0)
+	chain.Chain = append(chain.Chain, block)
 
 	return &block
 }
 
-func (self *blockchain) NewTransaction(sender string, recipient string, amount int) int {
-	self.CurrentTransactions = append(self.CurrentTransactions, transaction{
+func (chain *blockchain) NewTransaction(sender string, recipient string, amount int) int {
+	chain.CurrentTransactions = append(chain.CurrentTransactions, transaction{
 		Sender:    sender,
 		Recipient: recipient,
 		Amount:    amount,
 	})
 
-	return self.LastBlock().Index + 1
+	return chain.LastBlock().Index + 1
 }
 
-func (self *blockchain) LastBlock() *block {
-	return &self.Chain[len(self.Chain)-1]
+func (chain *blockchain) LastBlock() *block {
+	return &chain.Chain[len(chain.Chain)-1]
 }
 
 func Hash(block *block) string {
